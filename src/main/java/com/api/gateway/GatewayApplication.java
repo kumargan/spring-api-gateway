21f6ab3882c51616b53f4e508eb0e5d1af8ddefa
Ganesh Kumar <kumar.ganesh242@gmail.com>
2023-12-17 23:36:25 +0530

    first drop

---
+++ src/main/java/com/api/gateway/GatewayApplication.java
@@ -0,0 +1,45 @@
+package com.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@RequestMapping("/circuitbreakerfallback")
+	public String circuitbreakerfallback() {
+		return "This is a fallback";
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("path_route", r -> r.path("/get")
+						.uri("http://www.iana.org/domains/special"))
+				.route("host_route", r -> r.host("*.myhost.org")
+						.uri("http://httpbin.org"))
+				.route("rewrite_route", r -> r.host("*.rewrite.org")
+						.filters(f -> f.rewritePath("/foo/(?<segment>.*)",
+								"/${segment}"))
+						.uri("http://httpbin.org"))
+				.route("circuitbreaker_route", r -> r.host("*.circuitbreaker.org")
+						.filters(f -> f.circuitBreaker(c -> c.setName("slowcmd")))
+						.uri("http://httpbin.org"))
+				.route("circuitbreaker_fallback_route", r -> r.host("*.circuitbreakerfallback.org")
+						.filters(f -> f.circuitBreaker(c -> c.setName("slowcmd").setFallbackUri("forward:/circuitbreakerfallback")))
+						.uri("http://httpbin.org"))
+				.route("websocket_route", r -> r.path("/echo")
+						.uri("ws://localhost:9000"))
+				.build();
+	}
+}
